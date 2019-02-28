a35308dc1bde5f63b069327758ed1be990cf809f
Cesar Reyes <car047@ucsd.edu>
2019-02-28 10:41:16 -0800

    Lab 8 changes

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'dE9EwDKcJSA1aRRULQLk9owfN7GhWcQp';
+
+	var map = L.mapquest.map('map', {
+		center: [32.88,-117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88,-117.236]).addTo(map);
 }
